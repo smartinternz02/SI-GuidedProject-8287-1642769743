c234de61f51175aee9fadb1560e026b40d53b576
sravani <sravanibalguri6@gmail.com>
2022-01-25 16:21:55 +0530

    updated project

--- CKD_app.py
+++ CKD_app.py
@@ -4,31 +4,29 @@ from flask import Flask, request, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('CKD.pkl','rb')) 
+model = pickle.load(open('CKD.pkl','rb'))
 
 @app.route('/')
 def home():
-    return render_template('home.html')
-@app.route('/Prediction',methods=['POST','GET'])
-def prediction():
-    return render_template('indexnew.html')
-@app.route('/Home',methods=['POST','GET'])
-def my_home():
-    return render_template('home.html')
+    return render_template('ckd.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict',methods=['POST','GET'])
 def predict():
     input_features = [float(x) for x in request.form.values()]
     features_value = [np.array(input_features)]
 
-    features_name = ['blood_urea','blood_glucose_random','coronary_artery_disease',
-    'anemia','pus_cell','red_blood_cells','diabetesmellitus','pedal_edema']
+    features_name = ['red_blood_cells','pus_cell','diabetesmellitus','pedal_edema','anemia','coronary_artery_disease','blood_glucose_random','blood_urea']
 
     df = pd.DataFrame(features_value, columns=features_name)
 
     output = model.predict(df)
+    print(output)
+   
 
-    return render_template('result.html',predict_text=output)
+    if output==1:
+         return render_template('ckd.html',prediction_text='You have CHRONIC KIDNEY DISEASE')
+    else:
+        return render_template('ckd.html',prediction_text='You do not have CHRONIC KIDNEY DISEASE')
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
